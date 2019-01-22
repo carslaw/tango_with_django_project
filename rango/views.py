ee3fda4ae40412190977bf0851c9bb73cc34587a
Hugh Carslaw <2314197c@student.gla.ac.uk>
2019-01-22 14:50:49 +0000

    Ex 6.3 editing Category view

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Page
+
+# Import the Category model
+from rango.models import Category
 
 def index(request):
+    # Query the database for a list of ALL cargeories currently stored
+    # Order the categories by no. of likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
 
-# Construct a dictionary to pass to the template engine as its context.
-# Note the key  boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
-# Return a rendered response to send to the client
-# We make use of the shortest function to make our lives easier.
-# Note that the first parameter is the template that we wish to use.
-    return render(request, 'rango/index.html', context = context_dict)
+    # Render the response and send it back!
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')#, context = context_dict)
+
+def show_category(request, category_name_slug):
+    # Create a context dictionary which we can pass
+    # to the template rendering engine.
+    context_dict = {}
+
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception
+        # So the .get() method retruns one model instance or raises an exception
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Retrieve all of the associated pages.
+        # Note that filter() will return a list of page objects or an empty list
+        pages = Page.objects.filter(category=category)
+        
