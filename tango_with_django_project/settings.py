70ccb86f47c6b06c6f9a9c13b646d320eeda1720
Hugh Carslaw <2314197c@student.gla.ac.uk>
2019-02-03 17:56:28 +0000

    Written up to 9.6 demo. Error with migrating and url.

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        # 'OPTIONS': {'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -109,6 +110,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Password hashing
+
+# PASSWORD_HASHERS = (
+#     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+#     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher'
+# )
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
